104780fff4ded14b1ff4eeede5f6d433d203504c
Jesus Fernandez <fernandezjodar.jesus@gmail.com>
2020-10-28 18:49:07 +0100

    Añadidos gráficos de lineas, barras, donughts y radar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonughtComponent } from './components/donught/donught.component';
+import { RadarComponent } from './components/radar/radar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonughtComponent,
+    RadarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
